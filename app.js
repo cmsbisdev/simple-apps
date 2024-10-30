4ad1b65fb414a38a5f9dcdc68e96d038c38020bc
egieharsa <egie.harsa@gmail.com>
2024-10-30 16:05:02 +0700

    modified apps.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mysql = require('mysql');
 const app = express()
+app.disable("x-powered-by");
+
 const path = require('path')
 require('dotenv').config();
 
